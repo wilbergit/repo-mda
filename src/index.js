4c8cc11916038fdf49b60aa7d8e4643d02da204b
wilbergit <wilberag1985@gmail.com>
2023-09-08 12:37:10 -0500

    probando

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 console.log('Hello world')
 import express from 'express'
 import ejs from 'ejs'
-import {dirname,join} from 'path'
+
+import {dirname,join} from 'path'    //ruta absoluta concatenada sin importar SO
 import { fileURLToPath } from 'url'
 import indexRoutes from './routes/index.js'
 
 const app = express()
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
-//console.log(__dirname)
-app.set('views',join(__dirname,'views'))
-app.set('view engine','ejs')
+const __dirname = dirname(fileURLToPath(import.meta.url)) //ruta absoluta para poder mover el pro
+app.set('views',join(__dirname,'views'))  //donde estan las rutas concatenadas son impotar SO
+
+app.set('view engine','ejs')              //se configura view ejs
 app.use(indexRoutes)
 
 app.listen(process.env.PORT || 3000)
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express"
 const router = Router()
 
-router.get('/',(req,res)=> res.render('index',{tittle:'Master Dentar Arts...'}))
-router.get('/about',(req,res)=> res.render('about'))
-router.get('/contact',(req,res)=> res.render('contact'))
+router.get('/',(req,res)=> res.render('index',{title: 'Master Dental Arts...'}))  //envia variable tittle con valor
+
+router.get('/about',(req,res)=> res.render('about',{title: 'About'}))
+
+router.get('/contact',(req,res)=> res.render('contact',{title: 'Contact'}))
 
 export default router
